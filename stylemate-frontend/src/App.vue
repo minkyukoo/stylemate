3698e01943c3c7c11cfd81e22429f6c96e45cdd3
Bikramjitdivii <bikramjit@divii.com>
2022-01-12 23:22:22 +0530

    add

--- stylemate-frontend/src/App.vue
+++ stylemate-frontend/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app-container">
-    <transition name="fade">
-      <router-view></router-view>
-    </transition>
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
--- stylemate-frontend/src/components/NotFound.vue
+++ stylemate-frontend/src/components/NotFound.vue
@@ -15,43 +15,43 @@ export default {
 }
 </script>
 
-// <style lang="scss" scoped>
-// .pagenotfound {
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   flex-direction: column;
-//   height: 100vh;
-//   padding: 30px;
-//   .title {
-//     font-weight: bold;
-//     font-size: 43px;
-//     line-height: 59px;
-//     text-align: center;
-//   }
-//   .num-wrap {
-//     text-align: center;
-//     margin-bottom: 48px;
-//     .num {
-//       font-size: 100px;
-//       color: #757575;
-//     }
-//     p {
-//       font-size: 20px;
-//       color: #757575;
-//     }
-//   }
-//   .homebtn {
-//     @include flex-justify-center;
-//     background-color: $orange-1;
-//     color: $white-1;
-//     height: 58px;
-//     font-size: 24px;
-//     line-height: 33px;
-//     border-radius: 5px;
-//     min-width: 260px;
-//     max-width: 380px;
-//     width: 100%;
-//   }
-// }
-// </style>
+<style lang="scss" scoped>
+.pagenotfound {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  padding: 30px;
+  .title {
+    font-weight: bold;
+    font-size: 43px;
+    line-height: 59px;
+    text-align: center;
+  }
+  .num-wrap {
+    text-align: center;
+    margin-bottom: 48px;
+    .num {
+      font-size: 100px;
+      color: #757575;
+    }
+    p {
+      font-size: 20px;
+      color: #757575;
+    }
+  }
+  .homebtn {
+    @include flex-justify-center;
+    background-color: $black-1;
+    color: $white-1;
+    height: 58px;
+    font-size: 24px;
+    line-height: 33px;
+    border-radius: 5px;
+    min-width: 260px;
+    max-width: 380px;
+    width: 100%;
+  }
+}
+</style>
