631df689862b7df18444528207072997fd5e4f96
samreen-Hanriver <samreen@divii.com>
2022-01-19 19:44:30 +0530

    Brandlist

--- stylemate-frontend/src/views/pages/BrandList.vue
+++ stylemate-frontend/src/views/pages/BrandList.vue
@@ -1,10 +1,54 @@
 <template>
-<div class="width:40%">
-   <ion-card class="maincard">
-  <img src="@/assets/images/Rectangle1.png" alt="ion">
+<div class="main">
+   <ion-card class="maincard" >
+  <ion-item><img src="@/assets/images/Rectangle1.png" class="imgsec" alt="ion"></ion-item>
   <ion-card-header>
-    <ion-card-title>Hotel Panama Garden <img src="@/assets/icons/Vector.svg" alt=""  slot="end"></ion-card-title>
+    <ion-card-title>Hotel Panama Garden  <ion-icon :icon="heart" /> 
 
+    <!-- <img src="@/assets/icons/Vector.svg" alt="" slot="end"> -->
+
+    </ion-card-title>
+  </ion-card-header>
+  <ion-card-content class="maincontent">
+    ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.
+  </ion-card-content>
+   <ion-card-content class="subcontent">
+    ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices. 
+  </ion-card-content>
+</ion-card>
+
+
+
+   <ion-card class="maincard">
+  <img src="@/assets/images/Rectangle2.png" class="imgsec" alt="ion">
+  <ion-card-header>
+  <ion-card-title>Hotel Panama Garden  <ion-icon :icon="heart" /></ion-card-title>
+  </ion-card-header>
+  <ion-card-content class="maincontent">
+    ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.
+  </ion-card-content>
+   <ion-card-content class="subcontent">
+    ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices. 
+  </ion-card-content>
+</ion-card>
+
+<ion-card class="maincard">
+  <ion-item><img src="@/assets/images/Rectangle3.png" class="imgsec" alt="ion"></ion-item>
+  <ion-card-header>
+    <ion-card-title>Hotel Panama Garden  <ion-icon :icon="heart" /></ion-card-title>
+  </ion-card-header>
+  <ion-card-content class="maincontent">
+    ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.
+  </ion-card-content>
+   <ion-card-content class="subcontent">
+    ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices. 
+  </ion-card-content>
+</ion-card>
+
+<ion-card class="maincard">
+  <img src="@/assets/images/Rectangle4.png" class="imgsec" alt="ion">
+  <ion-card-header>
+    <ion-card-title>Hotel Panama Garden  <ion-icon :icon="heart" /></ion-card-title>
   </ion-card-header>
   <ion-card-content class="maincontent">
     ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.
@@ -18,16 +62,53 @@
 </template>
 
 <script>
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, } from '@ionic/vue';
-//import { pin, walk, warning, wifi, wine } from 'ionicons/icons';
-import { defineComponent } from 'vue';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle,IonItem } from '@ionic/vue';
+import { heart  } from 'ionicons/icons';
 
-export default defineComponent({
-  components: { IonCard, IonCardContent, IonCardHeader, IonCardTitle },
+export default {
+  name:'BrandList',
+  components: { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItem },
   setup() {
-    return { };
+    return { heart};
+  },
+  data(){
+    return{
+      data:[
+      {
+         "src":'https://source.unsplash.com/random/800x400?i=1',
+         "title":"Hotel Panama Garden",
+         "maincontent":"ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.",
+         "subcontent":"ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ultrices.",
+      },
+      {
+         "src":'https://source.unsplash.com/random/800x400?i=1',
+         "title":"Panama Garden",
+         "maincontent":"ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.",
+         "subcontent":"ipsum dolor, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.",
+      },
+      {
+         "src":'https://source.unsplash.com/random/800x400?i=1',
+         "title":"Hotel Panama",
+         "maincontent":"ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.",
+         "subcontent":"ipsum dolor sit amet, consectetur. Phasellus iaculis mollis ligula sed ultrices.",
+      },
+      {
+         "src":'https://source.unsplash.com/random/800x400?i=1',
+         "title":"Hotel Panama Garden value",
+         "maincontent":"ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.",
+         "subcontent":"ipsum dolor sit amet, consectetur adipiscing elit. iaculis mollis ligula sed ultrices.",
+      },
+      {
+         "src":'https://source.unsplash.com/random/800x400?i=1',
+         "title":"Hotel Panama Garden amet",
+         "maincontent":"ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.",
+         "subcontent":"ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ligula sed ultrices.",
+      }
+   ]
+    }
+
   }
-});
+}
 </script>
 <style>
 .subcontent{
@@ -40,6 +121,29 @@ font-size: 10px;
   color: #25282B;
 }
 .miancard{
-  width: 40%;
+ width: 0px;
+  height: 150px;
+  background-color: #eee;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  justify-self: center;
+  align-self: center;
+}
+.main{
+  width: fit-content;
+display: grid;
+  grid-gap: 10px;
+  grid-template-rows: repeat(auto-fit, minmax(150px, 1fr));
+  padding: center;
+}
+.imgsec{
+  width: 100%;
+  
+}
+img:hover {
+    background-color: rgb(36, 29, 29);
+    border-color: rgb(63, 13, 110);
+    border: #25282B;
 }
 </style>
