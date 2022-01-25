c410ad805b4748179c9201762c5cdf553a2e7ef2
samreen-Hanriver <samreen@divii.com>
2022-01-25 19:52:30 +0530

    Login Page

--- stylemate-frontend/src/views/pages/Login.vue
+++ stylemate-frontend/src/views/pages/Login.vue
@@ -1,13 +1,47 @@
 <template>
-  <i
+<ion-header>
+    <ion-toolbar>
+      <ion-title>Login</ion-title>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-content class="ion-padding">
+    <h1>Please login in Stylemate</h1>
+     <ion-button expand="block" fill="outline" color="dark">Login</ion-button>
+<ion-card>
+  <ion-item><h2>Customer service center</h2></ion-item>
+   <ion-item><h1>Notice</h1></ion-item>
+    <ion-item><h1>FAQ</h1></ion-item>
+     <ion-item><h1>1:1 inquiry</h1></ion-item>
+       <ion-item><h1>Terms of Use</h1></ion-item>
+         <ion-item><h1>Privacy Policy</h1></ion-item>
+</ion-card>
+  </ion-content>
 </template>
 
 <script>
+import { IonCard,
+  IonHeader,
+  IonTitle,
+  IonContent , IonItem ,IonButton} from '@ionic/vue';
   export default {
     name: 'Login',
+      components: {
+    
+    IonHeader,
+    IonTitle,
+    IonContent,
+IonCard,
+ IonItem ,
+IonButton
+
+  },
   }
 </script>
 
-<style lang="scss" scoped>
+<style>
+.border_input{
+  border-block:groove ;
+}
 
 </style>
