cd7f0dbc427d8609a0257312fa76484ade33a53c
Bikramjitdivii <bikramjit@divii.com>
2022-01-26 20:52:09 +0530

    Update TabsPage.vue

--- stylemate-frontend/src/views/TabsPage.vue
+++ stylemate-frontend/src/views/TabsPage.vue
@@ -1,29 +1,34 @@
 <template>
-  <ion-page>
+  <ion-page class="main-container relative">
     <ion-tabs>
       <TopNav></TopNav>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar >
         <ion-tab-button tab="tab1" href="/tabs/home">
-          <ion-icon :icon="triangle" />
+           <ion-icon :src="require(`@/assets/icons/home.svg`)" />
           <ion-label>Home</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Item</ion-label>
+         <ion-icon :src="require(`@/assets/icons/item.svg`)" />
+          <ion-label>ITEM</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :src="require(`@/assets/icons/brand.svg`)" />
           <ion-label>BRAND</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab4" href="/tabs/tab4">
-          <!-- <ion-icon :icon="heart" /> -->
-          <ion-icon :src="require(`@/assets/icons/home.svg`)" />
+          <ion-icon :src="require(`@/assets/icons/contents.svg`)" />
           <ion-label>CONTENTS</ion-label>
         </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :src="require(`@/assets/icons/mypage.svg`)" />
+          <ion-label>MY PAGE</ion-label>
+        </ion-tab-button>
+
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -47,7 +52,14 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+.main-container {
+  max-width: 500px;
+  min-width: 360px;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+}
 page-tabs .tabs .tabbar {
     background: #121212;
     width: 90% !important;
