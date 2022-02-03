3b64763dfadd206f55a54001f29aacfd2bbe32a4
samreen-Hanriver <samreen@divii.com>
2022-02-03 13:53:37 +0530

    Dynamic Content page

--- stylemate-frontend/src/views/pages/ContentPage.vue
+++ stylemate-frontend/src/views/pages/ContentPage.vue
@@ -9,116 +9,40 @@
   <!-- page content -->
   <ion-content :fullscreen="true">
     <ion-grid>
-      <div>
       <h1 class="title">BEST CONTENTS</h1>
       <br />
       <!-- Content row start -->
-      <ion-row >
-        <ion-col size-md="7">
-          <img src="@/assets/images/Rectanglec1.png" alt="" class="imagesize"/>
-        </ion-col>
-        <ion-col size-md="4">
-          <ion-row>
-            <img src="@/assets/images/Rectanglec2.png" alt="" class="io"/>
-          </ion-row><br/>
-          <ion-row>
-            <img src="@/assets/images/Rectanglec3.png" alt="" class="io"/>
-          </ion-row>
-        </ion-col>
-      </ion-row> 
-<br/>
-      <ion-row>
-        <ion-col>
-          <img src="@/assets/images/Rectanglec4.png" alt="" class="io"/>
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglec5.png" alt="" class="io"/>
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglec6.png" alt="" class="io"/>
-        </ion-col>
-      </ion-row>
-
-      <ion-row>
-        <ion-col size-md="4">
-          <ion-row>
-            <img src="@/assets/images/Rectanglec7.png" alt="" class="io"/>
-          </ion-row><br/>
-          <ion-row>
-            <img src="@/assets/images/Rectanglec8.png" alt="" class="io"/>
-          </ion-row>
-        </ion-col>
-        <ion-col size-md="7">
-          <img src="@/assets/images/Rectanglec9.png" alt=""  class="imagesize"/>
-        </ion-col>
-      </ion-row>
-      </div><br/>
+      <div>
+        <ion-row>
+          <ion-col
+            sizeLg="4"
+            sizeMd="4"
+            sizeXs="6"
+            v-for="image in img"
+            :key="image"
+          >
+            <img :src="image.src" v-bind:alt="img" @click="openModal" />
+          </ion-col>
+        </ion-row>
+      </div>
+      <br />
       <!-- Content row Ends -->
 
-<div>
-      <h1 class="title">NEW</h1>
-      <br />
-      <ion-row>
-        <!-- New content start -->
-        <ion-col>
-          <img
-            src="@/assets/images/Rectanglen1.png"
-            alt="NewImage"
-            class="io"
-          />
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen2.png" alt="NewImage"   class="io"/>
-        </ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen3.png" alt="NewImage" class="io"/>
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen4.png" alt="NewImage" class="io" />
-        </ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen5.png" alt="NewImage"  class="io"/>
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen6.png" alt="NewImage"  class="io"/>
-        </ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen7.png" alt="NewImage"  class="io"/>
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen8.png" alt="NewImage" class="io"/>
-        </ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen9.png" alt="NewImage"  class="io"/>
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen10.png" alt="NewImage" class="io" />
-        </ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen11.png" alt="NewImage" class="io" />
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen12.png" alt="NewImage" class="io"/>
-        </ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen13.png" alt="NewImage" class="io"/>
-        </ion-col>
-        <ion-col>
-          <img src="@/assets/images/Rectanglen14.png" alt="NewImage" class="io"/>
-        </ion-col>
-      </ion-row>
+      <div>
+        <h1 class="title">NEW</h1>
+        <br />
+         <!-- New content start -->
+        <ion-row>
+          <ion-col
+            sizeLg="4"
+            sizeMd="4"
+            sizeXs="6"
+            v-for="image in img_new"
+            :key="image"
+          >
+            <img :src="image.srci" v-bind:alt="img" @click="openModal" />
+          </ion-col>
+        </ion-row>
       </div>
       <!-- New content ends -->
     </ion-grid>
@@ -126,9 +50,16 @@
   <!-- End page content -->
 </template>
 <script>
-import { IonContent, IonCol, IonGrid, IonRow } from "@ionic/vue";
+import {
+  IonContent,
+  IonCol,
+  IonGrid,
+  IonRow,
+  modalController,
+} from "@ionic/vue";
 //import ExploreContainer from "@/components/ExploreContainer.vue";
 //import TopNav from "@/components/TopNav.vue";
+import ContentDetails from "@/components/ContentDetails.vue";
 export default {
   name: "ContentPage",
   components: { IonContent, IonCol, IonGrid, IonRow },
@@ -145,14 +76,40 @@ export default {
         { src: "https://source.unsplash.com/random/800x400?i=1" },
       ],
       img_new: [
-        { src: "https://source.unsplash.com/random/800x400?i=1" },
-        { src: "https://source.unsplash.com/random/800x400?i=1" },
-        { src: "https://source.unsplash.com/random/800x400?i=1" },
-        { src: "https://source.unsplash.com/random/800x400?i=1" },
-        { src: "https://source.unsplash.com/random/800x400?i=1" },
-        { src: "https://source.unsplash.com/random/800x400?i=1" },
+        {
+          src: "@/assets/images/Rectanglen1.png",
+          srci: "https://source.unsplash.com/random/800x400?i=1",
+        },
+        {
+          src: "@/assets/images/Rectanglen2.png",
+          srci: "https://source.unsplash.com/random/800x400?i=2",
+        },
+        {
+          src: "@/assets/images/Rectanglen3.png",
+          srci: "https://source.unsplash.com/random/800x400?i=1",
+        },
+        {
+          src: "@/assets/images/Rectanglen4.png",
+          srci: "https://source.unsplash.com/random/800x400?i=1",
+        },
+        {
+          src: "@/assets/images/Rectanglen5.png",
+          srci: "https://source.unsplash.com/random/800x400?i=1",
+        },
+        {
+          src: "@/assets/images/Rectanglen6.png",
+          srci: "https://source.unsplash.com/random/800x400?i=1",
+        },
       ],
     };
+  },
+  methods: {
+    async openModal() {
+      const modal = await modalController.create({
+        component: ContentDetails,
+      });
+      return modal.present();
+    },
   },
 };
 </script>
@@ -185,18 +142,11 @@ export default {
     background-repeat: no-repeat;
     display: block;
 } */
-/* img{
-  position:absolute;
-  top:0;
-  bottom:0;
-  left:0;
-  right:0;
-} */
-.io{
+.io {
   width: 100%;
 }
-.imagesize{
+.imagesize {
   height: 100%;
+  width: 100%;
 }
-
 </style>
