33a21de32137d03e24702a2d0d5f6cb27e6537d9
BratatiDasHanriver <63699333+BratatiDasHanriver@users.noreply.github.com>
2022-02-07 15:57:47 +0530

    add img

--- stylemate-frontend/src/components/CardItem.vue
+++ stylemate-frontend/src/components/CardItem.vue
@@ -1,10 +1,26 @@
 <template>
-    <h2>this is cards</h2>
+  <div class="main">
+    <ul>
+        <li>
+            <figure><img src="@/assets/images/Rectangle-2.jpg"></figure>
+        </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CardItem',
-    components: 'CardItem',
-}
+  name: "CardItem",
+  components: "CardItem",
+};
 </script>
+
+<style scoped>
+.main {
+  width: fit-content;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-rows: repeat(auto-fit, minmax(150px, 1fr));
+  padding: center;
+}
+</style>
