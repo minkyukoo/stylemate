c7616f0422888a55bab23840f0080f076e841aaa
samreen-Hanriver <samreen@divii.com>
2022-02-07 21:25:46 +0530

    Brand dynamic

---
+++ stylemate-frontend/src/components/AddChannels.vue
@@ -0,0 +1,25 @@
+<template>
+  <ion-page>
+    <!-- header -->
+
+    <!-- End header -->
+    <!-- page content -->
+    <ion-content :fullscreen="true">
+          <ion-card>
+    <ion-item>
+         <ion-button fill="outline" slot="end">View</ion-button>
+    </ion-item>
+    </ion-card>
+
+    </ion-content>
+    <!-- End page content -->
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonContent , IonCard , IonItem } from "@ionic/vue";
+export default {
+  name: "AddChannels",
+  components: { IonContent, IonPage, IonCard , IonItem },
+};
+</script>
--- stylemate-frontend/src/components/ChannelLink.vue
+++ stylemate-frontend/src/components/ChannelLink.vue
@@ -3,19 +3,25 @@
       <!-- header -->
     <!-- End header -->
     <!-- page content -->
-      <ion-content>
-          <div>
+      <ion-content :fullscreen="true">
+        
 <h2> Channel Connection</h2>
-<i class="fal fa-sync"></i>
-          </div>
+<ion-icon :src= "fingerPrint "> </ion-icon>
+        
       </ion-content>
   </ion-page>
 </template>
 <script>
 import { IonPage, IonContent } from "@ionic/vue";
+import { fingerPrint } from "ionicons/icons"
 export default {
   name: 'ChannelLink',
   components: { IonContent, IonPage, },
+  setup(){
+return{
+  fingerPrint 
+}
+  },
   methods: {
 }
 }
