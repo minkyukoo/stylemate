1a5b9967a9852bc06ec1cbade658d01987091b3c
Saikat Chakraborty <saikat@divii.com>
2022-02-08 19:35:33 +0530

    Home page conversion done

--- stylemate-frontend/src/views/Home.vue
+++ stylemate-frontend/src/views/Home.vue
@@ -8,145 +8,170 @@
        <!-- <ion-button router-link="/mypage">mypage</ion-button> -->
       <!-- <ExploreContainer name="Home page" /> -->
       <!-- <Tab1List /> -->
-      <ion-slides pager="true" :options="slideOpts" class="mainslide">
-        <ion-slide>
-          <img src="@/assets/images/main-banner-1.png">
-          <!-- <div class="bannerCont">
-            <h1>IDUN</h1>
-            <p>MINERALS<span>Stockholm</span></p>
-          </div> -->
-        </ion-slide>
-        <ion-slide>
-          <img src="@/assets/images/main-banner-1.png">
-          <!-- <div class="bannerCont">
-            <h1>IDUN</h1>
-            <p>MINERALS<span>Stockholm</span></p>
-          </div> -->
-        </ion-slide>
-        <ion-slide>
-          <img src="@/assets/images/main-banner-1.png">
-          <!-- <div class="bannerCont">
-            <h1>IDUN</h1>
-            <p>MINERALS<span>Stockholm</span></p>
-          </div> -->
-        </ion-slide>
-      </ion-slides>
+      <div class="mainslide">
+        <ion-slides pager="true" :options="slideOpts">
+          <ion-slide>
+            <img src="@/assets/images/main-banner-1.png">
+            <!-- <div class="bannerCont">
+              <h1>IDUN</h1>
+              <p>MINERALS<span>Stockholm</span></p>
+            </div> -->
+          </ion-slide>
+          <ion-slide>
+            <img src="@/assets/images/main-banner-1.png">
+            <!-- <div class="bannerCont">
+              <h1>IDUN</h1>
+              <p>MINERALS<span>Stockholm</span></p>
+            </div> -->
+          </ion-slide>
+          <ion-slide>
+            <img src="@/assets/images/main-banner-1.png">
+            <!-- <div class="bannerCont">
+              <h1>IDUN</h1>
+              <p>MINERALS<span>Stockholm</span></p>
+            </div> -->
+          </ion-slide>
+        </ion-slides>
+      </div>
+      <div class="overlapSlide">
+        <div class="headerLine">
+          <h4>NEW ITEM</h4>
+        </div>
+        <ion-slides pager="true" :options="multiSlideOpts">
+          <ion-slide>
+            <div class="multiSlideWrap">
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+            </div>
+          </ion-slide>
+          <ion-slide>
+            <div class="multiSlideWrap">
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+            </div>
+          </ion-slide>
+          <ion-slide>
+            <div class="multiSlideWrap">
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+              <div class="slideItem">
+                <img src="@/assets/images/main-item1.jpg">
+                <h4>Areuban</h4>
+                <p>리플렉 오버핏 조거 스웨...</p>
+                <span>#street fashion #sufa #hoodie</span>
+              </div>
+            </div>
+          </ion-slide>
+        </ion-slides>
+      </div>
+      
 
-      <ion-slides pager="true" :options="multiSlideOpts" class="overlapSlide">
-        <ion-slide>
-          <div class="multiSlideWrap">
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
+      <div class="headerLine">
+        <h4>LOOKBOOK</h4>
+        <span><img src="@/assets/icons/arrow-right.svg"></span>
+      </div>
+      <div class="lookBookMain">
+        <div class="bookLabel1 pattern1">
+          <img src="@/assets/images/book1.png">
+        </div>
+        <div class="bookLabel1 pattern2">
+          <div>
+            <img src="@/assets/images/Rectanglec1.png">
           </div>
-        </ion-slide>
-        <ion-slide>
-          <div class="multiSlideWrap">
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-          </div>
-        </ion-slide>
-        <ion-slide>
-          <div class="multiSlideWrap">
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-            <div class="slideItem">
-              <img src="@/assets/images/main-item1.jpg">
-              <h4>Areuban</h4>
-              <p>리플렉 오버핏 조거 스웨...</p>
-              <span>#street fashion #sufa #hoodie</span>
-            </div>
-          </div>
-        </ion-slide>
-      </ion-slides>
-
-        <div class="lookBookMain">
-          <div class="bookLabel1 pattern1">
-            <img src="@/assets/images/book1.png">
-          </div>
-          <div class="bookLabel1 pattern2">
-            <div>
-              <img src="@/assets/images/Rectanglec1.png">
-            </div>
-            <div>
-              <img src="@/assets/images/Rectanglec1.png">
-            </div>
+          <div>
+            <img src="@/assets/images/Rectanglec1.png">
           </div>
         </div>
+      </div>
 
-        <div class="lookBookMain">
-          <div class="bookLabel1 pattern2">
-            <div>
-              <img src="@/assets/images/Rectanglec1.png">
-            </div>
-            <div>
-              <img src="@/assets/images/Rectanglec1.png">
-            </div>
+      <div class="lookBookMain">
+        <div class="bookLabel2">
+          <div>
+            <img src="@/assets/images/Rectanglec1.png">
           </div>
-          <div class="bookLabel1 pattern1">
-            <img src="@/assets/images/book1.png">
+          <div>
+            <img src="@/assets/images/Rectanglec1.png">
+          </div>
+          <div>
+            <img src="@/assets/images/Rectanglec1.png">
           </div>
         </div>
+      </div>
 
+      <div class="lookBookMain">
+        <div class="bookLabel1 pattern2">
+          <div>
+            <img src="@/assets/images/Rectanglec1.png">
+          </div>
+          <div>
+            <img src="@/assets/images/Rectanglec1.png">
+          </div>
+        </div>
+        <div class="bookLabel1 pattern1">
+          <img src="@/assets/images/book1.png">
+        </div>
+      </div>
+      
     </ion-content>
     <!-- End page content -->
   </ion-page>
