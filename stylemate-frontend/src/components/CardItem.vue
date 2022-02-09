bbf0b57d9ddc01fe207085c4f77c986e7dbe1994
samreen-Hanriver <samreen@divii.com>
2022-02-09 18:39:36 +0530

    Items

--- stylemate-frontend/src/components/CardItem.vue
+++ stylemate-frontend/src/components/CardItem.vue
@@ -3,8 +3,8 @@
     <ion-slides :options="slideOpts">
       <ion-slide>
         <ul>
-          <li v-for="slide in slides" :key="slide">
-            <a href="#">{{ slide.title }}</a>
+          <li v-for="slide in categories_info" :key="slide">
+            <a href="#">{{ slide.name }}</a>
           </li>
         </ul>
       </ion-slide>
@@ -41,12 +41,12 @@
         </div>
         <ul v-if="layout === 'grid'" class="product-list grid-view">
           <li
-            v-for="(product, index) in products"
+            v-for="(product, index) in item_list"
             :key="index"
             class="product-list-item"
           >
             <figure>
-              <img src="@/assets/images/Rectangle-2.jpg" />
+              <img :src="product.imageThumbnailPath" />
               <div class="top-float-div">
                 <div class="social-icon">
                   <img src="@/assets/icons/instagram.svg" />
@@ -56,12 +56,12 @@
                 </div>
               </div>
             </figure>
-            <h3>{{ product.title }}</h3>
+            <!-- <h3>{{ product.title }}</h3> -->
             <p>{{ product.description }}</p>
             <!-- <span>{{ product.hashtags }}</span> -->
             <div class="hashWrap">
-              <span v-for="(hash, index) in product.hashtag" :key="index">{{
-                hash.name
+              <span v-for="(hash, index) in product.tag" :key="index">{{
+                hash.tag
               }}</span>
             </div>
           </li>
@@ -69,12 +69,12 @@
 
         <ul v-if="layout === 'list'" class="product-list list-view">
           <li
-            v-for="(product, index) in products"
+            v-for="(product, index) in item_list"
             :key="index"
             class="product-list-item"
           >
             <figure>
-              <img src="@/assets/images/Rectangle-2.jpg" />
+              <img :src="product.imageThumbnailPath" />
               <div class="top-float-div">
                 <div class="social-icon">
                   <img src="@/assets/icons/instagram.svg" />
@@ -83,16 +83,16 @@
             </figure>
             <div class="desc-box">
               <div class="text-box">
-                <h3>{{ product.title }}</h3>
+                <h3></h3>
                 <div class="favorite">
                   <img src="@/assets/icons/heart-outline.svg" />
                 </div>
               </div>
               <p>{{ product.description }}</p>
-              <!-- <span>{{ product.hashtags }}</span> -->
+              <span>{{ product.hashtags }}</span>
               <div class="hashWrap">
-                <span v-for="(hash, index) in product.hashtag" :key="index">{{
-                  hash.name
+                <span v-for="(hash, index) in product.tag" :key="index">{{
+                  hash.tag
                 }}</span>
               </div>
             </div>
@@ -114,6 +114,7 @@ import {
   IonInfiniteScrollContent,
 } from "@ionic/vue";
 import { defineComponent } from "vue";
+import ItemService from '@/services/ItemService';
 
 export default defineComponent({
   name: "CardItem",
@@ -267,7 +268,30 @@ export default defineComponent({
         },
       ],
       layout: "grid",
+      categories_info:[],
+      item_list:[],
+      product_details:[]
     };
+  },
+   created() {
+    this.itemService = new ItemService();
+  },
+  mounted() {
+    // Slide title
+    this.itemService.getProductCategories().then((data) => {
+      console.log("categories_info",data)
+      this.categories_info = data;
+    });
+// Product list
+    this.itemService.getProductLsit().then((data)=>{
+      console.log("ItemList",data)
+this.item_list=data;
+// Product details
+ this.itemService.getProductDetails().then((data)=>{
+      console.log("ProductDetails",data)
+this.product_details=data;
+    })
+    })
   },
 });
 </script>
---
+++ stylemate-frontend/src/services/ItemService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export default class ItemService {
+  async getProductCategories() {
+    return await axios.get(`/stylemates/categories`).then((res) => res.data.data);
+  }
+
+   async getProductLsit(){
+ return await axios.get(`/stylemates/products`).then((res) => res.data.data);
+   }
+
+   async getProductDetails(){
+    return await axios.get(`/stylemates/products/id`, { id:70 }).then((res) => res.data.data);
+   }
+  // async getItemsDetails() {
+  //   return await axios.get(`/stylemates/brands/id`, { id: 9 }).then((res) => res.data.data);
+  // }
+
+  // async viewUser(ids) {
+  //     return await axios.post(`/user/view/id`, { id: ids }).then((res) => res);
+  // }
+}
