7c666bdee99697478127d9fa00b21c1bd6f175e2
samreen-Hanriver <samreen@divii.com>
2022-02-10 21:49:53 +0530

    filter brand

--- stylemate-frontend/src/services/BrandService.js
+++ stylemate-frontend/src/services/BrandService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default class BrandService {
   async getBrandList() {
     return await axios.get(`/stylemates/brands`).then((res) => res.data.data);
+    
   }
   // async getBrandDetails() {
   //   return await axios.get(`/stylemates/brands/id`, { id: 9 }).then((res) => res.data.data);
