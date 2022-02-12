eabac19c57004c8964d206f4983b603feaf7bb0e
Bikramjit Show <bikramjit@divii.com>
2022-02-12 13:24:59 +0530

    Update BrandService.js

--- stylemate-frontend/src/services/BrandService.js
+++ stylemate-frontend/src/services/BrandService.js
@@ -5,9 +5,9 @@ export default class BrandService {
     return await axios.get(`/stylemates/brands`).then((res) => res.data.data);
     
   }
-  // async getBrandDetails() {
-  //   return await axios.get(`/stylemates/brands/id`, { id: 9 }).then((res) => res.data.data);
-  // }
+  async getBrandDetails() {
+    return await axios.get(`/stylemates/brands/id`, { id: 9 }).then((res) => res.data.data);
+  }
 
   // async viewUser(ids) {
   //     return await axios.post(`/user/view/id`, { id: ids }).then((res) => res);
