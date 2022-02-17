dcf609f4432f6b177d84aeb684eff64045d01278
diptendudivii <99716713+diptendudivii@users.noreply.github.com>
2022-02-17 19:09:14 +0530

    style update

--- stylemate-frontend/src/views/pages/Notifications.vue
+++ stylemate-frontend/src/views/pages/Notifications.vue
@@ -4,17 +4,39 @@
     <TopNav headerTitle="알림"></TopNav>
     <!-- End header -->
     <!-- page content -->
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true" part="scroll">
       <div class="inner-container">
         <!-- <DropDown class="pad-t-32" /> -->
         <div class="notiWrap">
-          <vue-select
-            placeholder="전체알림"
-            :options="options"> </vue-select>
+          <vue-select placeholder="전체알림" :options="options"> </vue-select>
         </div>
         <ul class="loopList">
-          <li>
-            <span class="circle"><img src="@/assets/icons/calender.svg" /></span>
+          <li v-for="item in notifications" :key="item.id">
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
+            <div class="loopInner">
+              <span>{{ humanReadableFormat(item.createdAt) }}</span>
+              <b>{{ item.title }}</b>
+              <p>{{ item.message }}</p>
+              <a href="#">바로가기 <img src="@/assets/icons/smallarw.png" /></a>
+            </div>
+          </li>
+          <!-- <li>
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
+            <div class="loopInner">
+              <span>2021년 01월 26일</span>
+              <b>포스트 등록 요청</b>
+              <p>협찬요청이 승인되었습니다. 포스트 등록을 진행해주세요</p>
+              <a href="#">바로가기 <img src="@/assets/icons/smallarw.png" /></a>
+            </div>
+          </li> -->
+          <!-- <li>
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
             <div class="loopInner">
               <span>2021년 01월 26일</span>
               <b>포스트 등록 요청</b>
@@ -23,7 +45,9 @@
             </div>
           </li>
           <li>
-            <span class="circle"><img src="@/assets/icons/calender.svg" /></span>
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
             <div class="loopInner">
               <span>2021년 01월 26일</span>
               <b>포스트 등록 요청</b>
@@ -31,6 +55,50 @@
               <a href="#">바로가기 <img src="@/assets/icons/smallarw.png" /></a>
             </div>
           </li>
+          <li>
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
+            <div class="loopInner">
+              <span>2021년 01월 26일</span>
+              <b>포스트 등록 요청</b>
+              <p>협찬요청이 승인되었습니다. 포스트 등록을 진행해주세요</p>
+              <a href="#">바로가기 <img src="@/assets/icons/smallarw.png" /></a>
+            </div>
+          </li>
+          <li>
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
+            <div class="loopInner">
+              <span>2021년 01월 26일</span>
+              <b>포스트 등록 요청</b>
+              <p>협찬요청이 승인되었습니다. 포스트 등록을 진행해주세요</p>
+              <a href="#">바로가기 <img src="@/assets/icons/smallarw.png" /></a>
+            </div>
+          </li>
+          <li>
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
+            <div class="loopInner">
+              <span>2021년 01월 26일</span>
+              <b>포스트 등록 요청</b>
+              <p>협찬요청이 승인되었습니다. 포스트 등록을 진행해주세요</p>
+              <a href="#">바로가기 <img src="@/assets/icons/smallarw.png" /></a>
+            </div>
+          </li>
+          <li>
+            <span class="circle"
+              ><img src="@/assets/icons/calender.svg"
+            /></span>
+            <div class="loopInner">
+              <span>2021년 01월 26일</span>
+              <b>포스트 등록 요청</b>
+              <p>협찬요청이 승인되었습니다. 포스트 등록을 진행해주세요</p>
+              <a href="#">바로가기 <img src="@/assets/icons/smallarw.png" /></a>
+            </div>
+          </li> -->
         </ul>
       </div>
     </ion-content>
@@ -43,12 +111,12 @@ import { IonPage, IonContent } from "@ionic/vue";
 import TopNav from "@/components/TopNav.vue";
 // import DropDown from "@/components/utilities/DropDown.vue";
 
-import VueNextSelect from 'vue-next-select';
+import VueNextSelect from "vue-next-select";
 import UserInfoService from "@/services/UserInfoService";
 
 export default {
   name: "Notifications",
-  components: { TopNav, IonContent, IonPage, 'vue-select': VueNextSelect, },
+  components: { TopNav, IonContent, IonPage, "vue-select": VueNextSelect },
   data() {
     return {
       notifications: [],
@@ -86,13 +154,18 @@ export default {
     },
   },
   setup() {
-        // const selectedItem = ref("Black");
+    // const selectedItem = ref("Black");
 
-        const options = [
-           'all', 'Campaign', 'Selection', 'Sample Post Registration', 'Edit sample post', 'Post registration'
-        ];
-        return { options };
-    },
+    const options = [
+      "all",
+      "Campaign",
+      "Selection",
+      "Sample Post Registration",
+      "Edit sample post",
+      "Post registration",
+    ];
+    return { options };
+  },
 };
 </script>
 
@@ -105,24 +178,24 @@ export default {
   margin: 0 auto;
 }
 
-.notiWrap{
-    margin-top: 30px;
+.notiWrap {
+  margin-top: 30px;
 }
-.notiWrap .vue-select{
-    width: 100%;
-    border: 1px solid #C4C4C4;
-    border-radius: 6px;
+.notiWrap .vue-select {
+  width: 100%;
+  border: 1px solid #c4c4c4;
+  border-radius: 6px;
 }
-.loopList{
+.loopList {
   margin-top: 32px;
 }
-.loopList li{
+.loopList li {
   position: relative;
   padding: 0 0 60px 24px;
   margin-left: 20px;
 }
-.loopList li::before{
-  border: 1px solid  #E4E4E7;
+.loopList li::before {
+  border: 1px solid #e4e4e7;
   content: "";
   left: 0;
   top: -32px;
@@ -130,8 +203,8 @@ export default {
   height: 32px;
   z-index: 1;
 }
-.loopList li::after{
-  border: 1px solid  #E4E4E7;
+.loopList li::after {
+  border: 1px solid #e4e4e7;
   content: "";
   left: 0;
   top: 0;
@@ -139,7 +212,7 @@ export default {
   height: 100%;
   z-index: 1;
 }
-.loopList li .circle{
+.loopList li .circle {
   position: absolute;
   left: -12px;
   top: 0;
@@ -152,35 +225,35 @@ export default {
   z-index: 2;
   justify-content: center;
 }
-.loopList li .loopInner span{
+.loopList li .loopInner span {
   font-size: 14px;
   font-weight: 400;
-  color: #25282B;
+  color: #25282b;
   margin-bottom: 16px;
   display: block;
 }
-.loopList li .loopInner b{
+.loopList li .loopInner b {
   font-size: 16px;
   font-weight: 700;
   color: #090909;
   margin-bottom: 5px;
   display: block;
 }
-.loopList li .loopInner p{
+.loopList li .loopInner p {
   font-size: 12px;
   font-weight: 400;
-  color: #25282B;
+  color: #25282b;
   margin-bottom: 10px;
 }
-.loopList li .loopInner a{
-  color: #C4C4C4;
+.loopList li .loopInner a {
+  color: #c4c4c4;
   font-size: 12px;
   font-weight: 400;
   display: flex;
   justify-content: flex-start;
   align-items: center;
 }
-.loopList li .loopInner a img{
+.loopList li .loopInner a img {
   margin: 0 0 0 10px;
   width: 5px;
   height: 9px;
