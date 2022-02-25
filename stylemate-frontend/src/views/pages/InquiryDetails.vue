cd6bddd2169e2c396fc22d59162e830ac58c4b8f
diptendudivii <99716713+diptendudivii@users.noreply.github.com>
2022-02-25 14:38:15 +0530

    Update InquiryDetails.vue

--- stylemate-frontend/src/views/pages/InquiryDetails.vue
+++ stylemate-frontend/src/views/pages/InquiryDetails.vue
@@ -10,25 +10,50 @@
           <h2>문의유형선택</h2>
 
           <div class="buttonGrp">
-            <button class="inq-btn active">협찬문의</button>
-            <button class="inq-btn">서비스 이용문의</button>
-            <button class="inq-btn">기타문의</button>
+            <button
+              @click="clickOption(1)"
+              :class="{ active: option === 1 }"
+              class="inq-btn"
+            >
+              협찬문의
+            </button>
+            <button
+              @click="clickOption(2)"
+              :class="{ active: option === 2 }"
+              class="inq-btn"
+            >
+              서비스 이용문의
+            </button>
+            <button
+              @click="clickOption(3)"
+              :class="{ active: option === 3 }"
+              class="inq-btn"
+            >
+              기타문의
+            </button>
           </div>
         </div>
         <div class="box">
           <h2>문의 내용</h2>
 
           <div class="form-box">
-              <div class="input-row">
-                  <input class="form-control" type="text" placeholder="제목을 입력해주세요.">
-              </div>
-              <div class="input-row">
-                  <textarea class="form-control" placeholder="내용을 입력해주세요."></textarea>
-              </div>
-              <div class="button-row">
-                  <button class="grey-btn">취소</button>
-                  <button class="black-btn">문의 등록</button>
-              </div>
+            <div class="input-row">
+              <input
+                class="form-control"
+                type="text"
+                placeholder="제목을 입력해주세요."
+              />
+            </div>
+            <div class="input-row">
+              <textarea
+                class="form-control"
+                placeholder="내용을 입력해주세요."
+              ></textarea>
+            </div>
+            <div class="button-row">
+              <button class="grey-btn">취소</button>
+              <button class="black-btn">문의 등록</button>
+            </div>
           </div>
         </div>
       </div>
@@ -44,6 +69,14 @@ import TopNav from "@/components/TopNav.vue";
 export default {
   name: "InquiryDetails",
   components: { TopNav, IonContent, IonPage },
+  data() {
+    return { option: null };
+  },
+  methods: {
+    clickOption(vl) {
+      this.option = vl;
+    },
+  },
 };
 </script>
 
@@ -58,25 +91,26 @@ export default {
   color: #212226;
   margin-bottom: 7px;
 }
-.box{
-    margin-top: 40px;
+.box {
+  margin-top: 40px;
 }
-.box:first-child, .input-row:first-child{
-    margin-top: 0;
+.box:first-child,
+.input-row:first-child {
+  margin-top: 0;
 }
-.form-box .form-control{
-    border: 1px solid #D4D4D8;
-    background: #FFFFFF;
-    border-radius: 8px;
-    padding: 12px 16px;
-    width: 100%;
+.form-box .form-control {
+  border: 1px solid #d4d4d8;
+  background: #ffffff;
+  border-radius: 8px;
+  padding: 12px 16px;
+  width: 100%;
 }
-textarea{
-    resize: none;
-    height: 204px;
+textarea {
+  resize: none;
+  height: 204px;
 }
-.input-row{
-    margin-top: 7px;
+.input-row {
+  margin-top: 7px;
 }
 .buttonGrp {
   display: flex;
@@ -99,31 +133,31 @@ textarea{
   font-weight: bold;
   background: #52525b;
 }
-.button-row{
-    display: flex;
-    margin-top: 42px;
+.button-row {
+  display: flex;
+  margin-top: 42px;
 }
-.button-row button{
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: 14px;
-    border-radius: 6px;
-    padding: 13px 20px;
-    flex: 1;
-    margin-left: 8px;
+.button-row button {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 14px;
+  border-radius: 6px;
+  padding: 13px 20px;
+  flex: 1;
+  margin-left: 8px;
 }
-.input-row:first-child{
-    margin-left: 0;
+.input-row:first-child {
+  margin-left: 0;
 }
-.button-row button.grey-btn{
-    background: #E4E4E7;
-    color: #A1A1AA;
+.button-row button.grey-btn {
+  background: #e4e4e7;
+  color: #a1a1aa;
 }
-.button-row button.black-btn{
-    background: #25282B;
-    color: #FFFFFF;
+.button-row button.black-btn {
+  background: #25282b;
+  color: #ffffff;
 }
 </style>
