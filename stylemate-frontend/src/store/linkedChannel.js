8415f3b92648941941044bfd1ce40f23551349e9
Bikramjit Show <bikramjit@divii.com>
2022-03-09 11:46:15 +0530

    add

--- stylemate-frontend/src/store/linkedChannel.js
+++ stylemate-frontend/src/store/linkedChannel.js
@@ -1,37 +1,21 @@
 import { reactive } from "vue";
-import axios from "axios";
+import ChannelService from "../services/ChannelService";
+import UserInfoService from "../services/UserInfoService";
+
+const channelService = new ChannelService();
+const userInfoService = new UserInfoService();
 
 const state = reactive({
   loginRes: undefined,
   isConnected: 'notconnected',
   fbDetails: undefined,
   igDetails: undefined,
+  extendToken: undefined,
 });
 
 
 
 const methods = {
-
-  // testData() {
-  //   // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-  //   console.log("Welcome!  Fetching your information.... ");
-  //   window.FB.api("/107832208496167?fields=name%2Cpicture", (response) => {
-  //     console.log('facebook: ', response);
-  //     state.fbDetails = response;
-  //   });
-  //   window.FB.api("/107832208496167/accounts?fields=instagram_business_account{id,name,username,profile_picture_url}", (response) => {
-  //     console.log('Instagram Channel: ', response.data);
-  //     state.igDetails = response.data;
-  //   });
-
-  //   window.FB.api("17841452123566228?fields=biography,ig_id,followers_count,follows_count,media_count,name,profile_picture_url,username", (response) => {
-  //     console.log('Instagram Channel 2: ', response);
-  //   });
-  //   window.FB.api("17988602920443231?fields=ig_id,media_type,media_product_type,media_url,permalink,shortcode,username,timestamp,like_count,comments_count,caption", (response) => {
-  //     console.log('Instagram Channel 3: ', response);
-  //   });
-
-  // },
 
   statusChangeCallback(response) {
     // Called with the results from FB.getLoginStatus().
@@ -43,7 +27,7 @@ const methods = {
       state.isConnected = response.status;
       // localStorage.setItem('fbaccessToken', response.authResponse.accessToken);
       localStorage.setItem('userID', response.authResponse.userID);
-     
+
     } else {
       // Not logged into your webpage or we are unable to tell.
       document.getElementById("status").innerHTML =
@@ -64,9 +48,11 @@ const methods = {
       if (response.authResponse) {
         // alert("You are logged in &amp; cookie set!");
         let ftoken = response.authResponse.accessToken;
-        this.igTokenExtend(ftoken).then((res) => {
-          console.log('igTokenExtend: ', res.data.token);
-          localStorage.setItem('fbaccessToken', res.data.token.access_token);
+        channelService.igTokenExtend(ftoken).then((res) => {
+          console.log('igTokenExtend: ', res.data);
+          // localStorage.setItem('fbaccessToken', res.data.token.access_token);
+          state.extendToken = res.data.token;
+          this.setIgrenewaltoken(res.data.token.access_token, res.data.token.token_type);
         });
         this.statusChangeCallback(response);
         return true;
@@ -77,55 +63,22 @@ const methods = {
         return false;
       }
     }, { scope: 'public_profile,instagram_basic,pages_show_list' });
-    // window.FB.getLoginStatus((response) => {
-    //   // See the onlogin handler
-    //   this.statusChangeCallback(response);
-    // });
     return false;
   },
 
-  async igTokenExtend(fbToken) {
-    return axios.get(`https://elsa.alloo.cc/commons/instagram-token`, {
-      params: {
-        token: fbToken,
-      },
+  setIgrenewaltoken(fextoken, fextokenName) {
+    userInfoService.getUserInfo().then((res) => {
+      console.log('getUserInfo: ', res.data);
+      let channelId = res.data.influence.channel[0].id;
+      let uid = res.data.uid;
+      let userId = res.data.id;
+      channelService.getIgrenewaltoken(uid,channelId,fextoken,userId,fextokenName).then((res) => {
+        console.log('igrenewaltoken: ', res.data);
+        localStorage.setItem('fbaccessToken', res.data.instagramChannel.accessToken);
+      });
     });
-  },
-
-  async getIgrenewaltoken(uid, channelId ) {
-    return await axios.patch(`/stylemates/users/${uid}/channels/${channelId}/instagram-renewal-token`, {
-      data: {
-        "access_token": "string",
-        "token_type": "string",
-        "expires_in": 0
-      },
-      headers: {
-        Authorization: 'Bearer ' + token //the token is a variable which holds the token
-      }
-    })
   }
 
-  // async initFacebook() {
-  //   window.fbAsyncInit = () => {
-  //     window.FB.init({
-  //       appId: "662067494654261", //You will need to change this
-  //       cookie: true, // This is important, it's not enabled by default
-  //       version: "v13.0",
-  //     });
-  //   };
-  // },
-
-  // async loadFacebookSDK(d, s, id) {
-  //   var js,
-  //     fjs = d.getElementsByTagName(s)[0];
-  //   if (d.getElementById(id)) {
-  //     return;
-  //   }
-  //   js = d.createElement(s);
-  //   js.id = id;
-  //   js.src = "https://connect.facebook.net/en_US/sdk.js";
-  //   fjs.parentNode.insertBefore(js, fjs);
-  // }
 
 };
 
