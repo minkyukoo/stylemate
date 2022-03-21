4ef4d806adaf2fe1f8b4803c27cf74194254cc1e
SammayadeepPal <sammayadeep@divii.com>
2022-03-21 10:09:53 +0530

    updating

--- stylemate-frontend/src/views/Home.vue
+++ stylemate-frontend/src/views/Home.vue
@@ -775,7 +775,7 @@ export default {
 }
 .brandSlider .headerLine h4 {
   color: #f6f6f6;
-  font-weight: 700;
+  
 }
 .brandSlider .multiSlideWrap {
   display: inherit;
@@ -871,7 +871,7 @@ export default {
 
 .brandDetails {
   background: #fff;
-  padding: 22px 16px;
+  padding: 22px 16px 24px 16px;
   min-height: 150px;
   cursor: pointer;
 }
