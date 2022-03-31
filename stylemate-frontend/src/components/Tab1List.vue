dd2c4ead8cef7af8cc38cde05762ad32d7bde470
BratatiDasHanriver <63699333+BratatiDasHanriver@users.noreply.github.com>
2022-03-31 11:39:46 +0530

    add

--- stylemate-frontend/src/components/Tab1List.vue
+++ stylemate-frontend/src/components/Tab1List.vue
@@ -1,108 +1,93 @@
 <template>
-  <ion-content>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-    <ion-list>
-      <ion-item key="1">
-        <ion-label>Pokémon Yellow</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="2">
-        <ion-label>Mega Man X</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="3">
-        <ion-label>The Legend of Zelda</ion-label>
-      </ion-item>
-      <ion-item key="4">
-        <ion-label>Pac-Man</ion-label>
-      </ion-item>
-      <ion-item key="5">
-        <ion-label>Super Mario World</ion-label>
-      </ion-item>
-      <ion-item key="1">
-        <ion-label>Pokémon Yellow</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="2">
-        <ion-label>Mega Man X</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="3">
-        <ion-label>The Legend of Zelda</ion-label>
-      </ion-item>
-      <ion-item key="4">
-        <ion-label>Pac-Man</ion-label>
-      </ion-item>
-      <ion-item key="5">
-        <ion-label>Super Mario World</ion-label>
-      </ion-item>
-      <ion-item key="1">
-        <ion-label>Pokémon Yellow</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="2">
-        <ion-label>Mega Man X</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="3">
-        <ion-label>The Legend of Zelda</ion-label>
-      </ion-item>
-      <ion-item key="4">
-        <ion-label>Pac-Man</ion-label>
-      </ion-item>
-      <ion-item key="5">
-        <ion-label>Super Mario World</ion-label>
-      </ion-item>
-      <ion-item key="1">
-        <ion-label>Pokémon Yellow</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="2">
-        <ion-label>Mega Man X</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="3">
-        <ion-label>The Legend of Zelda</ion-label>
-      </ion-item>
-      <ion-item key="4">
-        <ion-label>Pac-Man</ion-label>
-      </ion-item>
-      <ion-item key="5">
-        <ion-label>Super Mario World</ion-label>
-      </ion-item>
-      <ion-item key="1">
-        <ion-label>Pokémon Yellow</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="2">
-        <ion-label>Mega Man X</ion-label>
-        <ion-button router-link="tab1/list-details">Go to detail</ion-button>
-      </ion-item>
-      <ion-item key="3">
-        <ion-label>The Legend of Zelda</ion-label>
-      </ion-item>
-      <ion-item key="4">
-        <ion-label>Pac-Man</ion-label>
-      </ion-item>
-      <ion-item key="5">
-        <ion-label>Super Mario World</ion-label>
-      </ion-item>
-    </ion-list>
-  </ion-content>
+  <div class="main-container relative">
+    <div class="main-wrap">
+      <h1>hello world</h1>
+      <div class="sliderBox">
+        <swiper
+          :pagination="{
+            type: 'progressbar',
+          }"
+          :navigation="true"
+          :modules="modules"
+          class="mySwiper"
+        >
+          <swiper-slide v-for="(citem, i) of connectionInfo" :key="i + 1">
+            <div class="mainslide-banner-wrap">
+              <img :src="citem.imagePath" alt />
+            </div>
+            <div v-html="citem.body" class="ci-body"></div>
+            <!-- <h4>인스타그램을 실행 해 주세요</h4> -->
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { IonContent, IonButton } from '@ionic/vue';
+import { Pagination, EffectCoverflow, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { inject } from "vue";
+import ChannelService from "@/services/ChannelService";
+
 export default {
-  name: 'Tab1List',
-  components: { IonContent, IonButton }
-}
+  name: "Tab1List",
+  components: { Swiper, SwiperSlide },
+  setup() {
+    const store = inject("store");
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log("slide change");
+    };
+
+    return {
+      onSwiper,
+      onSlideChange,
+      modules: [Pagination, EffectCoverflow, Pagination, Navigation],
+      // modules: [EffectCoverflow, Pagination],
+      store,
+    };
+  },
+  data() {
+    return {
+      connectionInfo: null,
+    };
+  },
+  created() {
+    this.channelService = new ChannelService();
+
+  },
+  mounted() {
+    this.channelService.getAccountConnection().then((res) => {
+      console.log('getAccountConnection res', res);
+      this.connectionInfo = res.data.boardGuideDetail;
+    });
+  },
+  methods: {
+    accConnectConfirm() {
+      this.$router.push({
+        name: "NewMemberJoining",
+      });
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-h1 {
-  // color: red;
-  // color: $red;
+<style scoped>
+.sliderBox {
+  margin-top: 40px;
+  min-height: 500px;
+  height: 100%;
+}
+.sliderBox .mainslide-banner-wrap img {
+  margin: 0 auto;
+  max-width: 200px;
 }
 </style>
